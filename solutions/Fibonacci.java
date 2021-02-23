aa7b9eecae30023f09b0bb0c2f9a8920d2caf6f3
Kryukov Vladislav <284598@niuitmo.ru>
2021-02-23 14:48:51 +0300

    Fix Fibonacci

--- solutions/Fibonacci.java
+++ solutions/Fibonacci.java
@@ -1,61 +1,50 @@
 package solutions;
 
 public class Fibonacci {
-    public static final int[] fibs = new int[]{1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233};
+
+    public static final int[] fibs = new int[]{
+            1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597,
+            2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418,
+            317811, 514229, 832040, 1346269, 2178309, 3524578, 5702887, 9227465,
+            14930352, 24157817, 39088169, 63245986, 102334155, 165580141, 267914296,
+            433494437, 701408733, 1134903170, 1836311903
+    };
 
     public static double fibonacci(double a, double b, double l, double eps) {
-        double cnst = (b - a) / l;
+        double anchor = (b - a) / l;
         int n = -1;
         for (int i = 2; i < fibs.length; i++) {
-            if (fibs[i] > cnst) {
+            if (fibs[i] > anchor) {
                 n = i;
                 break;
             }
         }
+        if (n == -1) {
+            System.out.println("Cannot find F for this data. Choose different 'l'.");
+            return -1;
+        }
         double x1 = a + ((fibs[n - 2] * 1.0) / fibs[n]) * (b - a);
         double x2 = a + ((fibs[n - 1] * 1.0) / fibs[n]) * (b - a);
         int k = 1;
-        while (k < n) {
+        while (k < n - 1) {
             double f1 = Lab1.func(x1);
             double f2 = Lab1.func(x2);
             if (f1 > f2) {
-                // second step
                 a = x1;
                 x1 = x2;
                 x2 = a + ((fibs[n - k - 1] * 1.0) / fibs[n - k]) * (b - a);
-                if (k == n - 2) {
-                    // 5th
-                    x2 = x1 + eps;
-                    if (Lab1.func(x1) == Lab1.func(x2)) {
-                        a = x1;
-                    } else {
-                        b = x2;
-                    }
-                    break;
-                } else {
-                    // f(x2) and 4th
-                    k++;
-                }
             } else {
-                // third step
                 b = x2;
                 x2 = x1;
                 x1 = a + ((fibs[n - k - 2] * 1.0) / fibs[n - k]) * (b - a);
-                if (k == n - 2) {
-                    //5th
-                    x2 = x1 + eps;
-                    if (Lab1.func(x1) == Lab1.func(x2)) {
-                        a = x1;
-                    } else {
-                        b = x2;
-                    }
-                    break;
-                } else {
-                    // f(x1) and 4th
-                    k++;
-                }
             }
-
+            k++;
+        }
+        x2 = x1 + eps;
+        if (Math.abs(Lab1.func(x1) - Lab1.func(x2)) < eps) {
+            a = x1;
+        } else {
+            b = x2;
         }
         return (a + b) / 2;
     }
--- solutions/Lab1.java
+++ solutions/Lab1.java
@@ -2,7 +2,7 @@ package solutions;
 
 public class Lab1 {
     public static void main(String[] args) {
-        double fib = Fibonacci.fibonacci(0.5, 4, 0.05, 0.01);
+        double fib = Fibonacci.fibonacci(0.5, 4, 0.1, 0.1);
         System.out.println(fib);
     }
 
