7a6345a113d8e72e27a7543a22b647c857bdb54e
Dmitry <284629@niuitmo.ru>
2021-02-26 21:00:53 +0300

    Dichotomy fixed

--- solutions/Dichotomy.java
+++ solutions/Dichotomy.java
@@ -3,14 +3,18 @@ package solutions;
 import static solutions.Lab1.func;
 
 public class Dichotomy implements MinimizationMethod {
-    private double s = 0.000001;
+    private double s = 0.00000001;
 
-    public void setS(double s){
+    public void setS(double s) {
         this.s = s;
     }
+
     @Override
     public double findMin(double a, double b, final double eps) {
         double x1, x2;
+        if (s > eps) {
+            s = eps;
+        }
         do {
             x1 = (a + b - s) / 2;
             x2 = (a + b + s) / 2;
@@ -19,6 +23,7 @@ public class Dichotomy implements MinimizationMethod {
             } else {
                 a = x1;
             }
+            System.out.println(a + " " + b);
         } while ((b - a) / 2 >= eps);
         return (a + b) / 2;
     }
